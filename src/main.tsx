e35125dafe468c91355a0e139fa92851290bbbd5
Plínio Netto <plinio.ice.pns@gmail.com>
2023-07-27 13:46:06 -0300

    feat: implements react routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/home/Home.tsx';
 import { PrimeReactProvider } from 'primereact/api';
 import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
 
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import AppRoutes from './routes/Routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PrimeReactProvider>
-      <Home />
+      <AppRoutes />
     </PrimeReactProvider>
   </React.StrictMode>,
 );
