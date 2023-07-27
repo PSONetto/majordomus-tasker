3bf38a100a77e54351adbbfdc2b4cf9bd4401a64
Plínio Netto <plinio.ice.pns@gmail.com>
2023-07-27 02:56:11 -0300

    feat: implements PrimeReact, PrimeFlex and PrimeIcons

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/home/Home.tsx';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+
+import 'primereact/resources/primereact.min.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <PrimeReactProvider>
+      <Home />
+    </PrimeReactProvider>
   </React.StrictMode>,
 );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,14 @@
+import { Button } from 'primereact/button';
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    console.log('Adding');
+  }
+
+  return (
+    <main>
+      <Button label="Add" icon="pi pi-plus" onClick={handleClick} />
+    </main>
+  );
 }
